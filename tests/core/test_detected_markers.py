5a0cd27c38ec07a3e2e55e5e70b3b935abdff5cd
mnvoh <mnvoh90@gmail.com>
2024-03-22 14:00:19 +0000

    test: Add some more tests

--- tests/core/test_detected_markers.py
+++ tests/core/test_detected_markers.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-function-docstring,redefined-outer-name
+# pylint: disable=missing-function-docstring,redefined-outer-name,protected-access
 
 import pytest
 from cameratokeyboard.core.detected_objects import DetectedMarkers, Point
@@ -26,17 +26,29 @@ def detected_markers():
 def test_bottom_left_marker(detected_markers):
     assert detected_markers.bottom_left_marker == EXPECTED_MARKER_COORDINATES[0]
 
+    detected_markers._bl = None
+    assert detected_markers.bottom_left_marker is None
+
 
 def test_top_left_marker(detected_markers):
     assert detected_markers.top_left_marker == EXPECTED_MARKER_COORDINATES[1]
+
+    detected_markers._tl = None
+    assert detected_markers.top_left_marker is None
 
 
 def test_top_right_marker(detected_markers):
     assert detected_markers.top_right_marker == EXPECTED_MARKER_COORDINATES[2]
 
+    detected_markers._tr = None
+    assert detected_markers.top_right_marker is None
+
 
 def test_bottom_right_marker(detected_markers):
     assert detected_markers.bottom_right_marker == EXPECTED_MARKER_COORDINATES[3]
+
+    detected_markers._br = None
+    assert detected_markers.bottom_right_marker is None
 
 
 def test_all_marker_coordinates(detected_markers):

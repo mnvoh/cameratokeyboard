964cb53addea1e25a2aa764ab131a787dbfef40e
mnvoh <mnvoh90@gmail.com>
2024-03-22 12:04:03 +0000

    test: Implement tests for detected objects

---
+++ tests/core/test_detected_markers.py
@@ -0,0 +1,51 @@
+# pylint: disable=missing-function-docstring,redefined-outer-name
+
+import pytest
+from cameratokeyboard.core.detected_objects import DetectedMarkers, Point
+
+EXPECTED_MARKER_COORDINATES = [
+    Point(3, 6),
+    Point(33, 88),
+    Point(115, 142),
+    Point(164, 84),
+]
+
+
+@pytest.fixture
+def detected_markers():
+    # Create a sample list of bounding box coordinates
+    boxes = [
+        [10, 20, 30, 40],
+        [50, 60, 70, 80],
+        [90, 100, 110, 120],
+        [130, 140, 150, 160],
+    ]
+    return DetectedMarkers(boxes)
+
+
+def test_bottom_left_marker(detected_markers):
+    assert detected_markers.bottom_left_marker == EXPECTED_MARKER_COORDINATES[0]
+
+
+def test_top_left_marker(detected_markers):
+    assert detected_markers.top_left_marker == EXPECTED_MARKER_COORDINATES[1]
+
+
+def test_top_right_marker(detected_markers):
+    assert detected_markers.top_right_marker == EXPECTED_MARKER_COORDINATES[2]
+
+
+def test_bottom_right_marker(detected_markers):
+    assert detected_markers.bottom_right_marker == EXPECTED_MARKER_COORDINATES[3]
+
+
+def test_all_marker_coordinates(detected_markers):
+    assert detected_markers.all_marker_coordinates == EXPECTED_MARKER_COORDINATES
+
+
+def test_any_markers_missing(detected_markers):
+    assert not detected_markers.any_markers_missing
+
+
+def test_all_markers_present(detected_markers):
+    assert detected_markers.all_markers_present

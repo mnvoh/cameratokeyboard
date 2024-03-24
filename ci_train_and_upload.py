e497bbca9b820b5d6bfccef7ea8e0a77e6f0f1e2
mnvoh <mnvoh90@gmail.com>
2024-03-24 23:22:31 +0000

    fix: Fix AWS auth token expiring while the model training finished

--- ci_train_and_upload.py
+++ ci_train_and_upload.py
@@ -2,7 +2,9 @@
 
 import hashlib
 import os
+import shutil
 import sys
+import tempfile
 
 import boto3
 
@@ -43,7 +45,18 @@ def version_already_exists(version: str) -> bool:
     return objects and objects.get("KeyCount", 0) > 0
 
 
-def train() -> str:
+def train():
+    version = get_next_version()
+
+    if not version:
+        return
+
+    logger.info("Current data version: %s", version)
+
+    if version_already_exists(version):
+        logger.info("Model version %s already exists. Skipping the pipeline.", version)
+        return
+
     logger.info("Training the model")
 
     config = Config()
@@ -52,39 +65,34 @@ def train() -> str:
 
     results = trainer.run()
 
-    return os.path.join(results.save_dir, "weights", "best.pt")
+    model_path = os.path.join(results.save_dir, "weights", "best.pt")
+    dest_path = os.path.join(tempfile.tempdir, "c2k")
+    os.makedirs(dest_path, exist_ok=True)
+    shutil.copyfile(model_path, os.path.join(dest_path, f"{version}.pt"))
+
+    logger.info("Saved trained model to %s/%s.pt", dest_path, version)
 
 
-def upload_model(path: str, version: str):
-    remote_model_name = f"{version}.pt"
+def upload_model():
+    version = get_next_version()
+    model_name = f"{version}.pt"
+    model_path = os.path.join(tempfile.tempdir, "c2k", model_name)
     logger.info(
         "Uploading %s to s3://%s/%s/%s",
-        path,
+        model_path,
         BUCKET_NAME,
         REMOTE_MODELS_DIR,
-        remote_model_name,
+        model_name,
     )
 
-    s3_client.upload_file(path, BUCKET_NAME, f"{REMOTE_MODELS_DIR}/{remote_model_name}")
+    s3_client.upload_file(model_path, BUCKET_NAME, f"{REMOTE_MODELS_DIR}/{model_name}")
 
 
 def main():
-    current_version = get_next_version()
-
-    if not current_version:
-        sys.exit(0)
-
-    logger.info("Current data version: %s", current_version)
-    exists = version_already_exists(current_version)
-
-    if exists:
-        logger.info(
-            "Model version %s already exists. Skipping the pipeline.", current_version
-        )
-        return
-
-    trained_model_path = train()
-    upload_model(trained_model_path, current_version)
+    if sys.argv[1] == "train":
+        train()
+    elif sys.argv[1] == "upload":
+        upload_model()
 
 
 if __name__ == "__main__":
